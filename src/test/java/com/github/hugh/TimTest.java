fe4b00e71a7dd855d1329475df48df9b9a7c4e1a
HughWick <dang_star@live.com>
2020-08-31 23:33:11 +0800

    更新maven版本号

---
+++ src/test/java/com/github/hugh/TimTest.java
@@ -0,0 +1,17 @@
+package com.github.hugh;
+
+import com.github.hugh.util.TimeUtils;
+import org.junit.Test;
+
+/**
+ * @author hugh
+ * @version 1.0.0
+ */
+public class TimTest {
+
+    @Test
+    public void test01(){
+        System.out.println("--->>"+ TimeUtils.getTime());
+
+    }
+}
