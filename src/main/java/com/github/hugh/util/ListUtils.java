e7720daf56e24859e402450612994967cbf5387e
dang_star@live.com <hyT_Me2*277!.Jy>
2020-08-31 18:52:33 +0800

    移除List拆分方法

--- src/main/java/com/github/hugh/util/ListUtils.java
+++ src/main/java/com/github/hugh/util/ListUtils.java
@@ -96,31 +96,4 @@ public class ListUtils {
         }
         return source; // 返回已移除的结果集
     }
-
-    /**
-     * 将一个list均分成n个list,主要通过偏移量来实现的
-     *
-     * @param <T>    泛型
-     * @param source 数据源
-     * @param n      切割条数
-     * @return List
-     */
-    public static <T> List<List<T>> averageAssign(List<T> source, int n) {
-        List<List<T>> result = new ArrayList<>();
-        int remaider = source.size() % n; // (先计算出余数)
-        int number = source.size() / n; // 然后是商
-        int offset = 0;// 偏移量
-        for (int i = 0; i < n; i++) {
-            List<T> value = null;
-            if (remaider > 0) {
-                value = source.subList(i * number + offset, (i + 1) * number + offset + 1);
-                remaider--;
-                offset++;
-            } else {
-                value = source.subList(i * number + offset, (i + 1) * number + offset);
-            }
-            result.add(value);
-        }
-        return result;
-    }
 }
