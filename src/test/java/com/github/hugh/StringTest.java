fda89f2cce23f5ae52aa132e4da3f151ca823e19
Hugh <Dang_Star@live.com>
2020-09-14 15:57:15 +0800

    更新test

--- src/test/java/com/github/hugh/StringTest.java
+++ src/test/java/com/github/hugh/StringTest.java
@@ -12,6 +12,8 @@ public class StringTest {
     @Test
     public void test01(){
         String str = "https://github.com/HughWick/toolbox";
-        System.out.println("--->>>"+ StringUtils.before(str,"/"));
+        System.out.println("---1>>>"+ StringUtils.before(str,"/"));
+        System.out.println("--2->>>"+ StringUtils.isChinese(str));
+        System.out.println("--3->>>"+ StringUtils.getNumber(str));
     }
 }
