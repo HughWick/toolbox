3a111d440be508402d456abd4ee7f0563747092e
Hugh <Dang_Star@live.com>
2020-09-14 11:40:32 +0800

    新增排序字段校验注解

---
+++ src/main/java/com/github/hugh/constraints/Order.java
@@ -0,0 +1,49 @@
+package com.github.hugh.constraints;
+
+import com.github.hugh.constraints.validator.OrderValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * order 校验注解
+ *
+ * @author hugh
+ * @since 1.0.7
+ */
+@Target({METHOD, FIELD, PARAMETER})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = OrderValidator.class)
+public @interface Order {
+
+    /**
+     * 错误信息
+     * @return String
+     */
+    String message() default "排序类型不支持";
+
+    /**
+     * 接收字段
+     * @return String[]
+     */
+    String[] accepts() default {"desc", "asc"};
+
+    /**
+     * 默认值
+     * @return Class
+     */
+    Class<?>[] groups() default {};
+
+    /**
+     * 默认值
+     * @return Class
+     */
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/github/hugh/constraints/validator/OrderValidator.java
@@ -0,0 +1,31 @@
+package com.github.hugh.constraints.validator;
+
+import com.github.hugh.constraints.Order;
+import com.google.common.collect.Lists;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+/**
+ * order 字段校验
+ *
+ * @author huhg
+ * @since 1.0.7
+ */
+public class OrderValidator implements ConstraintValidator<Order, String> {
+    private List<String> valueList;
+
+    @Override
+    public void initialize(Order order) {
+        valueList = Lists.newArrayList();
+        for (String val : order.accepts()) {
+            valueList.add(val.toUpperCase());
+        }
+    }
+
+    @Override
+    public boolean isValid(String string, ConstraintValidatorContext constraintValidatorContext) {
+        return !valueList.contains(string.toUpperCase());
+    }
+}
