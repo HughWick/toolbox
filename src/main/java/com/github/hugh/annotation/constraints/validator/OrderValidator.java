29b3ee23d1d23c0d65ebb64a7e761ccef02bf844
HughWick <dang_star@live.com>
2020-09-17 22:37:56 +0800

    更新注释文档

--- src/main/java/com/github/hugh/annotation/constraints/validator/OrderValidator.java
+++ src/main/java/com/github/hugh/annotation/constraints/validator/OrderValidator.java
@@ -13,8 +13,8 @@ import java.util.List;
  * 注: 当项目启动后，会(懒加载)创建ConstraintValidator实例，在创建实例后会初始化调
  * 用{@link ConstraintValidator#initialize}方法。
  * 所以， 只有在第一次请求时，会走initialize方法， 后面的请求是不会走initialize方法的。
- * <p>
  * </p>
+ * <p>
  * 注: (懒加载)创建ConstraintValidator实例时， 会走缓存; 如果缓存中有，则直接使用相
  * 同的ConstraintValidator实例； 如果缓存中没有，那么会创建新的ConstraintValidator实例。
  * 由于缓存的key是能唯一定位的， 且 ConstraintValidator的实例属性只有在
