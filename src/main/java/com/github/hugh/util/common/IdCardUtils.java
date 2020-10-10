3469c1aa20a8c12dd7e7cc584ce450d3aad94f46
HughWick <dang_star@live.com>
2020-10-10 19:30:45 +0800

    更正方法

--- src/main/java/com/github/hugh/util/common/IdCardUtils.java
+++ src/main/java/com/github/hugh/util/common/IdCardUtils.java
@@ -13,6 +13,9 @@ public class IdCardUtils {
 
     /**
      * 验证所有的身份证的合法性
+     * <ul>
+     *     <li>该方法包含一代身份证15位的验证方法</li>
+     * </ul>
      *
      * @param idCard 身份证号码
      * @return boolean 合法返回true，否则返回false
@@ -25,6 +28,16 @@ public class IdCardUtils {
     }
 
     /**
+     * 字符串不是15位的身份证号码
+     *
+     * @param idCard 身份证号码
+     * @return boolean {@code true} 不合法
+     */
+    public static boolean isNotIdCard(String idCard) {
+        return !isIdCard(idCard);
+    }
+
+    /**
      * 校验字符串是18位的身份证号码
      *
      * @param idCard 身份证号码
@@ -32,6 +45,16 @@ public class IdCardUtils {
      */
     public static boolean is18Place(String idCard) {
         return IdCardValidator.is18Place(idCard);
+    }
+
+    /**
+     * 字符串不是18位的身份证号码
+     *
+     * @param idCard 身份证号码
+     * @return boolean {@code true} 不合法
+     */
+    public static boolean isNot18Place(String idCard) {
+        return !IdCardValidator.is18Place(idCard);
     }
 
     /**
--- src/test/java/com/github/hugh/common/IdCardTest.java
+++ src/test/java/com/github/hugh/common/IdCardTest.java
@@ -11,14 +11,16 @@ public class IdCardTest {
 
     @Test
     public void test01(){
-        String idcard15 = "420923198611046";
-//        String idcard18 = "110101199003071751";//
+//        String idcard15 = "632123820927051";
+        String idcard15 = "631111110927332";
         String idcard18 = "430104199003076214";//
         String hkIdCard = "H0905613601";//
         // 15位身份证
         System.out.println(IdCardUtils.isIdCard(idcard15));
+        System.out.println(IdCardUtils.isNotIdCard(idcard15));
         // 18位身份证
         System.out.println(IdCardUtils.is18Place(idcard18));
+        System.out.println(IdCardUtils.isNot18Place(idcard18));
         // 15位身份证转18位身份证
         System.out.println(IdCardUtils.encrypt(idcard18));
     }
