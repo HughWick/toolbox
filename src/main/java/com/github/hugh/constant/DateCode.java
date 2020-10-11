c6f45d3aa154279bafe9773490280e4264eca823
HughWick <dang_star@live.com>
2020-10-11 16:25:08 +0800

    更正文档

--- src/main/java/com/github/hugh/constant/DateCode.java
+++ src/main/java/com/github/hugh/constant/DateCode.java
@@ -4,40 +4,38 @@ package com.github.hugh.constant;
  * 常用日期格式
  *
  * @author hugh
+ * @since 1.0.0
  */
 public class DateCode {
 
     /**
-     * 去掉格式(-)符号后完整：年月
+     * [年-月]的日期格式
+     */
+    public static final String YEAR_MONTH = "yyyy-MM";
+
+    /**
+     * [年月]的日期格式
      */
     public static final String YEAR_MONTH_SIMPLE = "yyyyMM";
 
     /**
-     * 日期格式：年-月
-     */
-    public static final String YEAR_MONTH = "yyyy-MM";
-
-
-    /**
-     * 日期格式：年-月-日
+     * [年-月-日]的日期格式
      */
     public static final String YEAR_MONTH_DAY = "yyyy-MM-dd";
 
     /**
-     * 去掉格式符号后完整：年月日
+     * [年月日]的日期格式
      */
     public static final String YEAR_MONTH_DAY_SIMPLE = "yyyyMMdd";
 
 
     /**
-     * 完整的日期格式
-     * <p>年-月-日 时:分:秒</p>
+     * [年-月-日 时:分:秒]完整的日期格式
      */
     public static final String YEAR_MONTH_DAY_HOUR_MIN_SEC = "yyyy-MM-dd HH:mm:ss";
 
     /**
-     * 去掉格式符号后日期
-     * <p>年月日时分秒</p>
+     * [年月日时分秒]去掉格式符号后的完整日期格式
      */
     public static final String YEAR_MONTH_DAY_HOUR_MIN_SEC_SIMPLE = "yyyyMMddHHmmss";
 }
