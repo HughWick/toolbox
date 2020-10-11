ca60c264a61f7f839a7dc9cae69ed69962a27cd9
HughWick <dang_star@live.com>
2020-10-11 17:31:47 +0800

    枚举改用lombok

--- src/main/java/com/github/hugh/constant/enums/ControlEnum.java
+++ src/main/java/com/github/hugh/constant/enums/ControlEnum.java
@@ -1,40 +1,23 @@
 package com.github.hugh.constant.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
 /**
  * 控制状态枚举类
  *
  * @author hugh
  * @since 1.1.0
  */
+@ToString
+@Getter
+@AllArgsConstructor
 public enum ControlEnum {
 
     ENABLE("enable", "启用"),
     DISABLE("disable", "禁用");
 
-    private String code;
-    private String desc;
-
-    // 构造方法
-    ControlEnum(String code, String desc) {
-        this.code = code;
-        this.desc = desc;
-    }
-
-    /**
-     * 显示code信息
-     *
-     * @return String 字符串结果
-     */
-    public String code() {
-        return this.code;
-    }
-
-    /**
-     * 显示枚举描述信息
-     *
-     * @return String 字符串结果
-     */
-    public String desc() {
-        return this.desc;
-    }
+    private final String code;// 编码
+    private final String desc;// 中文描述
 }
--- src/test/java/com/github/hugh/base/BaseConvertTest.java
+++ src/test/java/com/github/hugh/base/BaseConvertTest.java
@@ -1,7 +1,7 @@
 package com.github.hugh.base;
 
+import com.github.hugh.constant.enums.ControlEnum;
 import com.github.hugh.util.base.BaseConvertUtils;
-import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -15,8 +15,17 @@ public class BaseConvertTest {
         System.out.println("=1==>>>>" + BaseConvertUtils.tenToSixteen(84213840));
         String binary = BaseConvertUtils.tenToBinary(84213840);
         System.out.println("==2=>>>>" + binary);
-        System.out.println("==3=>>>>" + BaseConvertUtils.toBinary(1024,12));
+        System.out.println("==3=>>>>" + BaseConvertUtils.toBinary(1024, 12));
         System.out.println("--4-===>>" + BaseConvertUtils.binaryToTen(binary));
         System.out.println("--4-===>>" + BaseConvertUtils.binaryToTen(binary));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("---1>>" + ControlEnum.ENABLE.getCode());
+        System.out.println("--2->>" + ControlEnum.ENABLE.getDesc());
+        System.out.println("--3->>" + ControlEnum.DISABLE.getDesc());
+        System.out.println("--4->>" + ControlEnum.DISABLE.getDesc());
+        System.out.println(ControlEnum.ENABLE);
+        System.out.println(ControlEnum.DISABLE);
     }
 }
