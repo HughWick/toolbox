4cc13f3ad2b7221b72f312d8a58cd6958c73ffda
Hugh <Dang_Star@live.com>
2021-02-09 16:37:37 +0800

    修正文档注释

--- src/main/java/com/github/hugh/support/JsonObjects.java
+++ src/main/java/com/github/hugh/support/JsonObjects.java
@@ -9,7 +9,7 @@ import com.google.gson.JsonObject;
  * 而后只需要使用get方法获取对象中的参数即可。
  *
  * @author hugh
- * @since 1.5.3
+ * @since 1.5.2
  */
 public class JsonObjects<T> extends JsonObjectUtils {
 
@@ -40,7 +40,7 @@ public class JsonObjects<T> extends JsonObjectUtils {
      * get{@link JsonArray}
      *
      * @param key 键
-     * @return {@link JsonArray}
+     * @return {@link JsonArray} json中获取key的{@link JsonArray} 值,没有返回{@code null}
      */
     public JsonArray getJsonArray(String key) {
         return getJsonArray(jsonObject, key);
@@ -50,7 +50,7 @@ public class JsonObjects<T> extends JsonObjectUtils {
      * 方便的方法获取{@link JsonObject}中对应key的{@link JsonObject}值。
      *
      * @param key 键
-     * @return {@link JsonObject}
+     * @return {@link JsonObject} json中获取key的JsonObject值,没有返回{@code null}
      */
     public JsonObject getJsonObject(String key) {
         return getJsonObject(jsonObject, key);
@@ -60,7 +60,7 @@ public class JsonObjects<T> extends JsonObjectUtils {
      * 方便的方法获取{@link JsonObject}中对应key的字符串值。
      *
      * @param key 键
-     * @return String
+     * @return String json中获取key的Integer值,没有返回{@code null}
      */
     public String getString(String key) {
         return getString(jsonObject, key);
@@ -70,7 +70,7 @@ public class JsonObjects<T> extends JsonObjectUtils {
      * 方便的方法获取{@link JsonObject}中对应key的int值。
      *
      * @param key 键
-     * @return int
+     * @return int json中获取key的Integer值,没有返回{@code 0}
      */
     public int getInt(String key) {
         return getInt(jsonObject, key);
@@ -80,7 +80,7 @@ public class JsonObjects<T> extends JsonObjectUtils {
      * 方便的方法获取{@link JsonObject}中对应key的double值。
      *
      * @param key 键
-     * @return double
+     * @return double json中获取key的Double值,没有返回{@code 0}
      */
     public double getDoubleValue(String key) {
         return getDoubleValue(jsonObject, key);
