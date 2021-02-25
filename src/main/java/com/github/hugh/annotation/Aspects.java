20e5389997f78163d41fc0d0e6182ad321eef406
Hugh <Dang_Star@live.com>
2021-02-25 12:43:11 +0800

    新增切面注解

---
+++ src/main/java/com/github/hugh/annotation/Aspects.java
@@ -0,0 +1,24 @@
+package com.github.hugh.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * aop拦截方法注解
+ *
+ * @author hugh
+ * @since 1.5.5
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Aspects {
+
+    /**
+     * 名称
+     *
+     * @return
+     */
+    String name() default "";
+}
