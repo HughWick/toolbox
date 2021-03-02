a797d88302eeadf1284251e2cd9919e23c0f4217
Hugh <Dang_Star@live.com>
2021-03-02 15:00:24 +0800

    补充文档注释

--- src/main/java/com/github/hugh/constant/EncryptCode.java
+++ src/main/java/com/github/hugh/constant/EncryptCode.java
@@ -1,11 +1,22 @@
 package com.github.hugh.constant;
 
 /**
- * @author AS
- * @date 2021/3/1 13:21
+ * 加密类型常量实体类
+ *
+ * @author hugh
+ * @since 1.5.7
  */
 public class EncryptCode {
+    /**
+     * md5
+     */
     public static final String MD5 = "MD5";
+    /**
+     * sha-256
+     */
     public static final String SHA_256 = "SHA-256";
+    /**
+     * sha-512
+     */
     public static final String SHA_512 = "SHA-512";
 }
