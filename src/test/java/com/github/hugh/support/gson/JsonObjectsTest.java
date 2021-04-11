019a935e70a84eb9b44cfa4a5bddcfc0f2758bac
HughWick <dang_star@live.com>
2021-04-11 12:42:57 +0800

    拆分JsonObject测试类

--- src/test/java/com/github/hugh/support/gson/JsonObjectsTest.java
+++ src/test/java/com/github/hugh/support/gson/JsonObjectsTest.java
@@ -1,12 +1,13 @@
 package com.github.hugh.support.gson;
 
-import com.github.hugh.util.gson.JsonObjectUtils;
 import com.github.hugh.util.gson.JsonObjects;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.junit.Test;
 
 /**
+ * gson JsonObjects测试类
+ *
  * @author AS
  */
 public class JsonObjectsTest {
@@ -81,20 +82,4 @@ public class JsonObjectsTest {
         System.out.println("--3->>" + new JsonObjects(json2.getJsonObject("data")));
     }
 
-    @Test
-    public void test04() {
-        JsonObjects json = new JsonObjects(null);
-        System.out.println("---1>>" + json.isNull());
-        System.out.println("----NED----");
-        String tem = "{\"code\":\"0000\",\"message\":\"xxxx\",\"data\":{\"list\":[{\"serialNo\":\"1339497989051277312\",\"createBy\":\"xxxx\",\"createDate\":1608196182000,\"updateBy\":\"xxxx\",\"updateDate\":1615444156000,\"deleteBy\":null,\"deleteDate\":null,\"deleteFlag\":0,\"identification\":0,\"counts\":null,\"tatal\":0,\"id\":489,\"ip\":\"169.1.1.1\",\"ssq_NO\":\"43010408\",\"sfmc\":\"\",\"csmc\":\"\",\"qxmc\":\"\",\"xzjdmc\":\"\",\"xqbm\":\"43010408\",\"sf\":\"430000\",\"cs\":\"430100\",\"qx\":\"430104\",\"xzjd\":\"43010408\",\"zcmc\":\"服务器1\",\"pp_ID\":3,\"ppmc\":\"前卫\",\"zxzt\":0,\"bdzt\":0,\"dzjcmc\":null,\"sbxlh\":null,\"kkmc\":null,\"rksj\":1608196182000,\"sxxsj\":1615444155000,\"tdmc\":null,\"gbbm\":null,\"gldk\":\"161\",\"ywzjbd_ID\":null,\"jd\":null,\"wd\":null,\"xxdz\":\"\",\"fx_ID\":0,\"kkbd_ID\":0,\"dzjcbd_ID\":null,\"zclxmc\":\"塔式服务器\",\"zclxmc1\":null,\"zclxmc2\":\"服务器（存储）\",\"zclx\":15,\"cj\":\"1\",\"xh\":\"cd212\",\"sydw\":1,\"zczt\":\"1\",\"dlzh\":null,\"dlmm\":null,\"ccrq\":1608134400000,\"azrq\":1606924800000,\"bxq\":2,\"sbbm\":null,\"ht_ID\":97,\"cjdw\":0,\"ywdw\":0,\"szs\":null,\"tbzt\":0,\"iccid\":null,\"ywzjzt\":null,\"mb\":3,\"cds\":null,\"zzzp\":\"[{\\\"name\\\":1609728432052,\\\"url\\\":\\\"https://cmmop.hnlot.com.cn/annex/cmmop/zczx/zcgl/ZiChanTuPian/image/2020/12/18/921313b0d11a4d038446401ee1a52754.jpg\\\",\\\"status\\\":\\\"success\\\",\\\"uid\\\":1610012972059},{\\\"name\\\":1609728432053,\\\"url\\\":\\\"https://cmmop.hnlot.com.cn/annex/cmmop/zczx/zcgl/ZiChanTuPian/image/2020/12/18/55b3c99aed424ca9973e0cf1ab12f420.jpg\\\",\\\"status\\\":\\\"success\\\",\\\"uid\\\":1610012972060},{\\\"name\\\":1609728432054,\\\"url\\\":\\\"https://cmmop.hnlot.com.cn/annex/cmmop/zczx/zcgl/ZiChanTuPian/image/2020/12/18/774507f4f08d4fd095e03c15b8018402.jpg\\\",\\\"status\\\":\\\"success\\\",\\\"uid\\\":1610012972061}]\",\"rkdjr_ID\":\"192\",\"kqjy\":0,\"kqtbjy\":0,\"fxmc\":null,\"gdzt\":null,\"zxzts\":null,\"ywzjbdsl\":0,\"laniotzt\":null,\"ywzjjcsj\":null,\"wtzhscsj\":null,\"wtscsj\":null,\"txmm\":null,\"imei\":null,\"htmc\":null,\"bdzcjh\":null,\"ywzjmc\":null,\"cjmc\":null}],\"countPage\":10,\"countList\":10}}\n";
-        JsonObjects jsonObjects = new JsonObjects(tem);
-        System.out.println("---->?>" + jsonObjects.isEquals("code", "0000"));
-//        String artr = "[{serialNo=1339497989051277312, createBy=谭运虎, createDate=1.608196182E12, updateBy=系统操作, updateDate=1.615445521E12, deleteBy=null, deleteDate=null, deleteFlag=0.0, identification=0.0, counts=null, tatal=0.0, id=489.0, ip=169.1.1.1, ssq_NO=43010408, sfmc=, csmc=, qxmc=, xzjdmc=, xqbm=43010408, sf=430000, cs=430100, qx=430104, xzjd=43010408, zcmc=服务器1, pp_ID=3.0, ppmc=前卫, zxzt=0.0, bdzt=0.0, dzjcmc=null, sbxlh=null, kkmc=null, rksj=1.608196182E12, sxxsj=1.61544552E12, tdmc=null, gbbm=null, gldk=161, ywzjbd_ID=null, jd=null, wd=null, xxdz=, fx_ID=0.0, kkbd_ID=0.0, dzjcbd_ID=null, zclxmc=塔式服务器, zclxmc1=null, zclxmc2=服务器（存储）, zclx=15.0, cj=1, xh=cd212, sydw=1.0, zczt=1, dlzh=null, dlmm=null, ccrq=1.6081344E12, azrq=1.6069248E12, bxq=2.0, sbbm=null, ht_ID=97.0, cjdw=0.0, ywdw=0.0, szs=null, tbzt=0.0, iccid=null, ywzjzt=null, mb=3.0, cds=null, zzzp=[{\"name\":1609728432052,\"url\":\"https://cmmop.hnlot.com.cn/annex/cmmop/zczx/zcgl/ZiChanTuPian/image/2020/12/18/921313b0d11a4d038446401ee1a52754.jpg\",\"status\":\"success\",\"uid\":1610012972059},{\"name\":1609728432053,\"url\":\"https://cmmop.hnlot.com.cn/annex/cmmop/zczx/zcgl/ZiChanTuPian/image/2020/12/18/55b3c99aed424ca9973e0cf1ab12f420.jpg\",\"status\":\"success\",\"uid\":1610012972060},{\"name\":1609728432054,\"url\":\"https://cmmop.hnlot.com.cn/annex/cmmop/zczx/zcgl/ZiChanTuPian/image/2020/12/18/774507f4f08d4fd095e03c15b8018402.jpg\",\"status\":\"success\",\"uid\":1610012972061}], rkdjr_ID=192, kqjy=0.0, kqtbjy=0.0, fxmc=null, gdzt=null, zxzts=null, ywzjbdsl=0.0, laniotzt=null, ywzjjcsj=null, wtzhscsj=null, wtscsj=null, txmm=null, imei=null, htmc=null, bdzcjh=null, ywzjmc=null, cjmc=null}]\n";
-        JsonObjects jsonObjects1 = jsonObjects.jsonObjects("data");
-        JsonArray list = jsonObjects1.getJsonArray("list");
-//        System.out.println("---->>" + );
-        String str2  =  "[{\"serialNo\":\"1339497989051277312\",\"createBy\":1,\"createDate\":1608196182000,\"updateBy\":\"xxxx\",\"updateDate\":1615444156000}]";
-        JsonArray jsonElements = JsonObjectUtils.parseArray(str2);
-        System.out.println("-====>>>" + JsonObjectUtils.toArrayList(jsonElements));
-    }
 }
