d52933718889b9d28cff2ce7117868878e92762f
HughWick <dang_star@live.com>
2021-04-21 21:09:16 +0800

    新增百度坐标转高德坐标、gps实体类

--- src/main/java/com/github/hugh/util/CoordinatesUtils.java
+++ src/main/java/com/github/hugh/util/CoordinatesUtils.java
@@ -1,5 +1,7 @@
 package com.github.hugh.util;
 
+import com.github.hugh.model.dto.GpsDTO;
+
 import java.text.DecimalFormat;
 
 /**
@@ -17,27 +19,52 @@ public class CoordinatesUtils {
     private static double pi = 3.14159265358979324;
 
     /**
+     * 计算后的Π
+     */
+    private static double CALC_PI = pi * 3000.0 / 180.0;
+
+    /**
+     * 截取小数点后八位
+     */
+    private final static DecimalFormat decimalFormat = new DecimalFormat("#.00000000");
+
+    /**
      * 高德转百度
      * <p>火星坐标系 (GCJ-02) 与百度坐标系 (BD-09) 坐标的转换算法</p>
      * <p>注：只保留小数点后八位</p>
      *
      * @param longitude 经度
      * @param latitude  纬度
-     * @return double[] {@code {经度,纬度}}
+     * @return {@link GpsDTO}
      * @since 1.6.3
      */
-    public static double[] gcj02ToBd09(final double longitude, final double latitude) {
-        double calcPi = pi * 3000.0 / 180.0;
-        double z = Math.sqrt(longitude * longitude + latitude * latitude) + 0.00002 * Math.sin(latitude * calcPi);
-        double theta = Math.atan2(latitude, longitude) + 0.000003 * Math.cos(longitude * calcPi);
+    public static GpsDTO gcj02ToBd09(final double longitude, final double latitude) {
+        double z = Math.sqrt(longitude * longitude + latitude * latitude) + 0.00002 * Math.sin(latitude * CALC_PI);
+        double theta = Math.atan2(latitude, longitude) + 0.000003 * Math.cos(longitude * CALC_PI);
         // 计算后的bd09 经度
-        double bdlon = z * Math.cos(theta) + 0.0065;
+        double bdlon = (z * Math.cos(theta) + 0.0065);
         // 金酸后的bd09 纬度
         double bdlat = z * Math.sin(theta) + 0.006;
-        DecimalFormat df = new DecimalFormat("#.00000000");
-        double[] bd_lat_lon = new double[2];
-        bd_lat_lon[0] = Double.parseDouble(df.format(bdlon));
-        bd_lat_lon[1] = Double.parseDouble(df.format(bdlat));
-        return bd_lat_lon;
+        return new GpsDTO(Double.parseDouble(decimalFormat.format(bdlat)), Double.parseDouble(decimalFormat.format(bdlon)));
+    }
+
+    /**
+     * 百度转高德
+     * <p>百度坐标(bd09ll) -> 火星坐标(gcj02ll)</p>
+     *
+     * @param longitude 经度
+     * @param latitude  纬度
+     * @return GpsDTO  {@link GpsDTO}
+     * @since 1.6.4
+     */
+    public static GpsDTO bd09ToGcj02(double longitude, double latitude) {
+        double x = longitude - 0.0065, y = latitude - 0.006;
+        double z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * CALC_PI);
+        double theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * CALC_PI);
+        // 计算后的gcj02ll 经度
+        String gcjLon = decimalFormat.format(z * Math.cos(theta));
+        // 计算后的gcj02ll 纬度
+        String gcjLat = decimalFormat.format(z * Math.sin(theta));
+        return new GpsDTO(Double.parseDouble(gcjLon), Double.parseDouble(gcjLat));
     }
 }
--- src/test/java/com/github/hugh/CoordinatesTest.java
+++ src/test/java/com/github/hugh/CoordinatesTest.java
@@ -1,7 +1,7 @@
 package com.github.hugh;
 
+import com.github.hugh.model.dto.GpsDTO;
 import com.github.hugh.util.CoordinatesUtils;
-import org.apache.commons.lang3.ArrayUtils;
 import org.junit.Test;
 
 /**
@@ -12,10 +12,20 @@ public class CoordinatesTest {
 
     @Test
     public void test01() {
-        double[] doubles = CoordinatesUtils.gcj02ToBd09(112.94671143, 28.21967801);
-        double[] doubles2 = CoordinatesUtils.gcj02ToBd09(0, 0);
-        System.out.println(ArrayUtils.toString(doubles, ","));
-        System.out.println("-1-->>" + doubles[0]);
-        System.out.println("-2-->>" + doubles[1]);
+        GpsDTO gpsDTO = CoordinatesUtils.gcj02ToBd09(112.94671143, 28.21967801);
+//        double[] doubles2 = CoordinatesUtils.gcj02ToBd09(0, 0);
+//        System.out.println(ArrayUtils.toString(doubles, ","));
+        System.out.println("-1-->>" + gpsDTO.getLongitude());
+        System.out.println("-2-->>" + gpsDTO.getLatitude());
     }
+
+    @Test
+    public void test02() {
+        GpsDTO gpsDTO =CoordinatesUtils.bd09ToGcj02(112.95321356, 28.22574022);
+//        double[] doubles2 = CoordinatesUtils.gcj02ToBd09(0, 0);
+//        System.out.println(ArrayUtils.toString(doubles, ","));
+        System.out.println("-1-->>" + gpsDTO.getLongitude());
+        System.out.println("-2-->>" + gpsDTO.getLatitude());
+    }
+
 }
