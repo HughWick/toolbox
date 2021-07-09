eb6198d4c46ed8f27e70554bb87aacebd0631e69
Hugh <Dang_Star@live.com>
2021-07-09 18:06:35 +0800

    完善进制转换工具类

--- src/main/java/com/github/hugh/util/base/BaseConvertUtils.java
+++ src/main/java/com/github/hugh/util/base/BaseConvertUtils.java
@@ -1,6 +1,9 @@
 package com.github.hugh.util.base;
 
+import com.github.hugh.constant.CharsetCode;
 import com.github.hugh.exception.ToolboxException;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * 进制转换工具类
@@ -158,4 +161,80 @@ public class BaseConvertUtils {
     public static String hexToTenString(String hex) {
         return String.valueOf(hexToTen(hex));
     }
+
+    /**
+     * 16进制数转byte
+     * @param hexString
+     * @return
+     */
+    public static byte hexStringToBytes(String hexString) {
+        hexString = hexString.toUpperCase();
+        int length = hexString.length() / 2;
+        char[] hexChars = hexString.toCharArray();
+        byte[] d = new byte[length];
+        for (int i = 0; i < length; i++) {
+            int pos = i * 2;
+            d[i] = (byte) (charToByte(hexChars[pos]) << 4 | charToByte(hexChars[pos + 1]));
+        }
+        return d[0];
+    }
+
+    private static byte charToByte(char c) {
+        return (byte) "0123456789ABCDEF".indexOf(c);
+    }
+
+
+    /**
+     * 16进制直接转换成为ascii字符串(无需Unicode解码)
+     *
+     * @param hexStr Byte字符串(Byte之间无分隔符
+     * @return 对应的字符串
+     * @author zcj
+     */
+    public static String hexToAscii(String hexStr) throws Exception {
+        hexStr = hexStr.replace(" ", "");
+        String str = "0123456789ABCDEF"; //16进制能用到的所有字符 0-15
+        char[] hexs = hexStr.toCharArray();//toCharArray() 方法将字符串转换为字符数组。
+        int length = (hexStr.length() / 2);//1个byte数值 -> 两个16进制字符
+        byte[] bytes = new byte[length];
+        int n;
+        for (int i = 0; i < bytes.length; i++) {
+            int position = i * 2;//两个16进制字符 -> 1个byte数值
+            n = str.indexOf(hexs[position]) * 16;
+            n += str.indexOf(hexs[position + 1]);
+            // 保持二进制补码的一致性 因为byte类型字符是8bit的  而int为32bit 会自动补齐高位1  所以与上0xFF之后可以保持高位一致性
+            //当byte要转化为int的时候，高的24位必然会补1，这样，其二进制补码其实已经不一致了，&0xff可以将高的24位置为0，低8位保持原样，这样做的目的就是为了保证二进制数据的一致性。
+            bytes[i] = (byte) (n & 0xff);
+        }
+        return new String(bytes, CharsetCode.GB_2312);
+    }
+
+    /**
+     *  * ascii字符串转换成为16进制(无需Unicode编码)
+     *  * @param str 待转换的ASCII字符串
+     *
+     * @author zcj
+     *  * @return byte字符串 （每个Byte之间空格分隔）
+     *  
+     */
+    public static String asciiToHex(String str) {
+        char[] chars = "0123456789ABCDEF".toCharArray();//toCharArray() 方法将字符串转换为字符数组。
+        StringBuilder sb = new StringBuilder(); //StringBuilder是一个类，可以用来处理字符串,sb.append()字符串相加效率高
+        byte[] bs = new byte[0];//String的getBytes()方法是得到一个操作系统默认的编码格式的字节数组
+        try {
+            bs = str.getBytes(CharsetCode.GB_2312);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        int bit;
+        for (byte b : bs) {
+            bit = (b & 0x0f0) >> 4; // 高4位, 与操作 1111 0000
+            sb.append(chars[bit]);
+            bit = b & 0x0f;  // 低四位, 与操作 0000 1111
+            sb.append(chars[bit]);
+            sb.append(' ');//每个Byte之间空格分隔
+        }
+        return sb.toString().trim();
+    }
+
 }
