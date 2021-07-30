77b76c3cd559730da7654f7fed30cb75c206ad51
Hugh <Dang_Star@live.com>
2021-07-30 18:26:58 +0800

    新增IP计算掩码测试代码工具

---
+++ src/main/java/com/github/hugh/annotation/constraints/Port.java
@@ -0,0 +1,50 @@
+package com.github.hugh.annotation.constraints;
+
+import com.github.hugh.annotation.constraints.validator.IpV4Validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * User: AS
+ * Date: 2021/7/30 10:09
+ */
+@Documented
+@Retention(RUNTIME)
+@Target({METHOD, FIELD, PARAMETER})
+@Constraint(validatedBy = IpV4Validator.class)//指向自定义验证类
+public @interface Port {
+
+
+    /**
+     * 返回信息
+     *
+     * @return String
+     */
+    String message() default "IP格式错误"; //这边可以标注默认的验证失败消息
+
+    /**
+     * 值
+     *
+     * @return String
+     */
+    String value() default "";
+
+    /**
+     * 是否允许为空
+     * <p>默认不允许，也就是false时，不管value是空字符串或者null都进行Ip规则校验</p>
+     *
+     * @return boolean
+     */
+    boolean nullable() default false;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
