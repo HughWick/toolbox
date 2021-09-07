00e63fa9fffe2b5a468e299a45d935f6a9ea1b96
dang_star@live.com <dang_star@live.com>
2021-09-07 21:12:56 +0800

    常量修改为final

--- src/main/java/com/github/hugh/constant/SuffixCode.java
+++ src/main/java/com/github/hugh/constant/SuffixCode.java
@@ -9,40 +9,40 @@ package com.github.hugh.constant;
 public class SuffixCode {
 
     //图片类型
-    public static String PNG = "PNG";
-    public static String JPG = "JPG";
-    public static String JPEG = "JPEG";
-    public static String BMP = "BMP";
-    public static String GIF = "GIF";
-    public static String SVG = "SVG";
+    public static final String PNG = "PNG";
+    public static final String JPG = "JPG";
+    public static final String JPEG = "JPEG";
+    public static final String BMP = "BMP";
+    public static final String GIF = "GIF";
+    public static final String SVG = "SVG";
     //PDF
-    public static String PDF = "PDF";
+    public static final String PDF = "PDF";
     //压缩文件类型
-    public static String ZIP = "ZIP";
-    public static String RAR = "RAR";
-    public static String GZ = "GZ";
-    public static String TYPE_7Z = "7Z";
+    public static final String ZIP = "ZIP";
+    public static final String RAR = "RAR";
+    public static final String GZ = "GZ";
+    public static final String TYPE_7Z = "7Z";
 
     /**
      * xls是excel2003及以前版本
      */
-    public static String XLS = "XLS";
+    public static final String XLS = "XLS";
 
     /**
      * EXCEL 2007以上版本的扩展名
      */
-    public static String XLSX = "XLSX";
+    public static final String XLSX = "XLSX";
 
     /**
      * word文档类型 Word2003以前版本的文本文档
      */
-    public static String DOC = "DOC";
+    public static final String DOC = "DOC";
 
     /**
      * docx是微软Word的文件扩展名 Microsoft Office2007之后
      */
-    public static String DOCX = "DOCX";
+    public static final String DOCX = "DOCX";
 
     // 视频
-    public static String MP4 = "MP4";
+    public static final String MP4 = "MP4";
 }
