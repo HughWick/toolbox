9ea514acad5b7fd603c659a978cd936fe8e961fe
Hugh <Dang_Star@live.com>
2021-12-07 17:40:11 +0800

    新增jdk ping内部方法

--- toolbox-core/src/test/java/com/github/hugh/PingTest.java
+++ toolbox-core/src/test/java/com/github/hugh/PingTest.java
@@ -2,8 +2,10 @@ package com.github.hugh;
 
 import com.github.hugh.util.PingUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.StopWatch;
 
 import java.io.IOException;
+import java.net.InetAddress;
 
 /**
  * @author AS
@@ -31,5 +33,29 @@ public class PingTest {
     public void test03() {
         System.out.println("---1>>" + PingUtils.ping("192.168.1.25"));
         System.out.println("-2-->>" + PingUtils.ping("192.168.1.236", 9, 1000));
+    }
+
+    public static void main(String[] args) {
+        try {
+            int count = 0;
+            int timeout = 4000;
+            String ip = "192.168.1.32";
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start("开始");
+            while (count < 3) {
+                System.out.println("=====开始=======");
+                InetAddress byName = InetAddress.getByName(ip);
+                boolean reachable = byName.isReachable(timeout);
+                System.out.println("==结果=>>>" + reachable);
+                if (reachable) {
+                    return;
+                }
+                count++;
+            }
+            stopWatch.stop();
+            System.out.println(stopWatch.prettyPrint());
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
     }
 }
