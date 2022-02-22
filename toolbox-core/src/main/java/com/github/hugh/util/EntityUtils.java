0082fbd03306f9c69fc26822a4e7ac93f2395fa3
Hugh <Dang_Star@live.com>
2022-02-22 10:33:13 +0800

    完善注释

--- toolbox-core/src/main/java/com/github/hugh/util/EntityUtils.java
+++ toolbox-core/src/main/java/com/github/hugh/util/EntityUtils.java
@@ -57,7 +57,7 @@ public class EntityUtils {
      * 复制list集合
      * <p>
      * 使用场景：Entity、Bo、Vo层数据的复制，因为BeanUtils.copyProperties只能给目标对象的属性赋值，却不能在List集合下循环赋值，因此添加该方法
-     * 如：List<AdminEntity> 赋值到 List<AdminVo> ，List<AdminVo>中的 AdminVo 属性都会被赋予到值
+     * 如：{@code List<AdminEntity> }赋值到 {@code List<AdminVo>} ，{@code List<AdminVo>}中的 AdminVo 属性都会被赋予到值
      * S: 数据源类 ，T: 目标类::new(eg: AdminVo::new)
      *
      * @param <T>      目标对象类型
