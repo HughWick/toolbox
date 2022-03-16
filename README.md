fc851a259b771b2018cab362a054a8590fbbdd3d
Hugh <Dang_Star@live.com>
2022-03-16 15:21:43 +0800

    升级到2.2.1

--- README.md
+++ README.md
@@ -40,6 +40,6 @@ JDK 11+
 <dependency>
     <groupId>com.github.hughwick</groupId>
     <artifactId>toolbox-all</artifactId>
-    <version>2.2.0</version>
+    <version>2.2.1</version>
 </dependency>
 ```
--- toolbox-crypto/src/test/java/com/github/hugh/crypto/Md5Test.java
+++ toolbox-crypto/src/test/java/com/github/hugh/crypto/Md5Test.java
@@ -12,10 +12,10 @@ public class Md5Test {
     public void md5(){
         System.out.println("--1->"+ Md5Utils.lowerCase("1234"));
         System.out.println("-2-->"+ Md5Utils.lowerCase("88888888"));
-        System.out.println("-2-->"+ Md5Utils.lowerCase("8566889"));
+        System.out.println("-3-->"+ Md5Utils.lowerCase("8566889"));
         //f854b68c6f8b2195704f76e05aaa65a1
-        System.out.println("-2-->"+ Md5Utils.lowerCase("huahua"));
-        System.out.println("-3-->"+ Md5Utils.upperCase("1234"));
+        System.out.println("-4-->"+ Md5Utils.lowerCase("huahua"));
+        System.out.println("-5-->"+ Md5Utils.upperCase("1234"));
     }
 
 }
