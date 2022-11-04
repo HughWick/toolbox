a7b92964c9a17192fe6c186cd567340ecdecd4ae
e <dang_star@live.com>
2022-11-04 15:55:24 +0800

    优化测试用例

--- toolbox-core/src/test/java/com/github/hugh/GzipUtilsTest.java
+++ toolbox-core/src/test/java/com/github/hugh/GzipUtilsTest.java
@@ -1,24 +1,28 @@
 package com.github.hugh;
 
 import com.github.hugh.util.GzipUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
+ * gzip 压缩
+ *
  * @author AS
  * @date 2020/9/3 11:19
  */
-public class GzipUtilsTest {
+class GzipUtilsTest {
 
-    public static void main(String[] args) {
+    @Test
+    void testLength() throws IOException {
         String str =
                 "《中文编码测试》间22lastUpdateTime%22%3A%222011-10-28+9%3A39%3A41%22%2C%22smsList%22%3A%5B%7B%22liveState%22%3A%221";
-        System.out.println("原长度：" + str.length());
-        try {
-            System.out.println("压缩后字符串：" + GzipUtils.compress(str).length());
-            String compress = GzipUtils.compress(str);
-            System.out.println("压缩后字符串：" + compress);
-            System.out.println("解压缩后字符串：" + GzipUtils.uncompress(compress));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        assertEquals(106, str.length());
+        String compress = GzipUtils.compress(str);
+        assertEquals(124, compress.length());
+        assertEquals(str, GzipUtils.uncompress(compress));
     }
 }
