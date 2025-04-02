3b1c6dec59076eb166b182354580152cf20946aa
HughWick <Dang_Star@live.com>
2025-04-02 16:02:45 +0800

    新增element casader级联处理三级结构

--- toolbox-core/src/test/java/com/github/hugh/support/tree/ProcessTreeData.java
+++ toolbox-core/src/test/java/com/github/hugh/support/tree/ProcessTreeData.java
@@ -290,4 +290,94 @@ public class ProcessTreeData {
         }
     }
 
+
+    public static void processCustomThree(List<TreeNodeObject> objects, List<TreeNode> rootList, List<TreeNode> childList) {
+        Map<String, Object> nodeMap = new HashMap<>();
+        Map<String, Object> nodeMap2 = new HashMap<>();
+        Map<String, Object> nodeMap3 = new HashMap<>();
+        // 将每一级都只放一条数据，并用上下级code码区分
+        for (TreeNodeObject treeNode : objects) {
+            // 存在省份
+            if (nodeMap.containsKey(treeNode.getProvinceCode())) {// 省份
+                if (nodeMap2.containsKey(treeNode.getCityCode())) { // 城 判断城市key是否存在
+                    if (nodeMap3.containsKey(treeNode.getAreaCode())) { // 区
+//                        if (nodeMap4.containsKey(treeNode.getStreetCodeEx())) { // 街道
+//                        } else {
+                            // 第四级
+//                            TreeNode childNode4 = new TreeNode();
+//                            childNode4.setId(treeNode.getStreetCodeEx());
+//                            childNode4.setParentId(treeNode.getAreaCode());
+//                            childNode4.setCustomLabel(treeNode.getStreetName());
+//                            childNode4.setCustomValue(treeNode.getStreetCodeEx());
+//                            childList.add(childNode4);
+//                            nodeMap4.put(treeNode.getStreetCodeEx(), true);
+//                        }
+                    } else {
+//                        // 保存一次下一级
+//                        TreeNode childNode4 = new TreeNode(treeNode.getStreetCodeEx(), treeNode.getAreaCode(), treeNode.getStreetName(), null);
+//                        TreeNode childNode4 = new TreeNode();
+//                        childNode4.setId(treeNode.getStreetCodeEx());
+//                        childNode4.setParentId(treeNode.getAreaCode());
+//                        childNode4.setCustomLabel(treeNode.getStreetName());
+//                        childNode4.setCustomValue(treeNode.getStreetCodeEx());
+//                        childList.add(childNode4);
+//                        nodeMap4.put(treeNode.getStreetCodeEx(), true);
+//                        TreeNode childNode3 = new TreeNode(treeNode.getAreaCode(), treeNode.getCityCode(), treeNode.getAreaName(), null);
+                        TreeNode childNode3 = new TreeNode();
+                        childNode3.setId(treeNode.getAreaCode());
+                        childNode3.setParentId(treeNode.getCityCode());
+                        childNode3.setCustomLabel(treeNode.getAreaName());
+                        childNode3.setCustomValue(treeNode.getAreaCode());
+                        childList.add(childNode3);
+                        nodeMap3.put(treeNode.getAreaCode(), true);
+                    }
+                } else { // 不存在则
+                    TreeNode childNode2 = new TreeNode();
+                    childNode2.setId(treeNode.getCityCode());
+                    childNode2.setParentId(treeNode.getProvinceCode());
+                    childNode2.setCustomLabel(treeNode.getCityName());
+                    childNode2.setCustomValue(treeNode.getCityCode());
+                    childList.add(childNode2);
+                    // 保存一次下一级
+                    TreeNode childNode3 = new TreeNode();
+                    childNode3.setId(treeNode.getAreaCode());
+                    childNode3.setParentId(treeNode.getAreaCode());
+                    childNode3.setCustomLabel(treeNode.getAreaName());
+                    childNode3.setCustomValue(treeNode.getAreaCode());
+                    childList.add(childNode3);
+                    nodeMap2.put(treeNode.getCityCode(), true);
+                }
+            } else { // 不存在
+                TreeNode rootNode = new TreeNode();
+                rootNode.setId(treeNode.getProvinceCode());
+                rootNode.setCustomLabel(treeNode.getProvinceName());
+                rootNode.setCustomValue(treeNode.getProvinceCode());
+                rootList.add(rootNode);
+                // 保存一次第二级
+                TreeNode childNode2 = new TreeNode();
+                childNode2.setId(treeNode.getCityCode());
+                childNode2.setParentId(treeNode.getProvinceCode());
+                childNode2.setCustomLabel(treeNode.getCityName());
+                childNode2.setCustomValue(treeNode.getCityCode());
+                childList.add(childNode2);
+                nodeMap2.put(treeNode.getCityCode(), true);
+                // 保存一次第三级
+                TreeNode childNode3 = new TreeNode();
+                childNode3.setId(treeNode.getAreaCode());
+                childNode3.setParentId(treeNode.getCityCode());
+                childNode3.setCustomLabel(treeNode.getAreaName());
+                childNode3.setCustomValue(treeNode.getAreaCode());
+                childList.add(childNode3);
+                nodeMap3.put(treeNode.getAreaCode(), true);
+                // 保存一次第四级
+//                TreeNode childNode4 = new TreeNode();
+//                childNode4.setId(treeNode.getStreetCodeEx());
+//                childNode4.setParentId(treeNode.getAreaCode());
+//                childNode4.setCustomLabel(treeNode.getStreetName());
+//                childNode4.setCustomValue(treeNode.getStreetCodeEx());
+//                childList.add(childNode4);
+                nodeMap.put(treeNode.getProvinceCode(), true);
+            }
+        }
+    }
 }
