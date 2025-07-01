4df25369da1623bb5d7962f4577cb80479be41f2
HughWick <Dang_Star@live.com>
2025-07-01 10:07:01 +0800

    完善测试用例，修改方法注释

--- toolbox-core/src/test/java/com/github/hugh/components/IpResolverTest.java
+++ toolbox-core/src/test/java/com/github/hugh/components/IpResolverTest.java
@@ -1,15 +1,17 @@
 package com.github.hugh.components;
 
+import com.github.hugh.bean.dto.Ip2regionDTO;
+import com.github.hugh.exception.ToolboxException;
 import com.github.hugh.util.io.StreamUtils;
 import com.github.hugh.util.ip.Ip2RegeinTest;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.function.Supplier;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * IP 解析测试工具
@@ -17,8 +19,6 @@ import static org.junit.Assert.assertNull;
  * @author AS
  * @date 2021/2/23 14:23
  */
-//@RunWith(PowerMockRunner.class)
-//@PrepareForTest({Ip2regionUtils.class})
 public class IpResolverTest {
 
     /**
@@ -52,6 +52,16 @@ public class IpResolverTest {
 //    }
 
     @Test
+    void getCompleteTest() {
+        String ip1 = "192.168.1.191";
+        final Ip2regionDTO str1 = IpResolver.on(ip1, easyRedisSupplier.get()).parse();
+        assertEquals("0", str1.getRegion());
+        String ip2 = "175.8.167.6";
+        final Ip2regionDTO str2 = IpResolver.on(ip2, easyRedisSupplier.get()).parse();
+        assertEquals("0", str2.getRegion());
+    }
+
+    @Test
     public void completeSpareTest() {
 //        String ip1 = "192.168.1.191";
 //        final String str1 = IpResolver.on(ip1).getComplete();
@@ -75,5 +85,14 @@ public class IpResolverTest {
         String ip3 = "154.18.161.64";
         final String str3 = IpResolver.on(ip3, easyRedisSupplier.get()).getSimple();
         assertNull(str3);
+    }
+
+    @Test
+    @DisplayName("Test case: parse() 方法返回 null，应抛出异常")
+    void testGetComplete_ParseReturnsNull_ThrowsException() {
+        IpResolver ipResolver = IpResolver.on("8.0.25.", easyRedisSupplier.get());
+        ToolboxException exception = assertThrows(ToolboxException.class, ipResolver::getComplete);
+        assertEquals("failed to create content cached searcher:", exception.getMessage());
+        assertEquals("invalid ip address `8.0.25.`" , exception.getCause().getMessage());
     }
 }
