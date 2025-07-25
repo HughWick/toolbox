440fb3f5f431807ba354edba07fffc6a2413a73a
HughWick <Dang_Star@live.com>
2025-07-25 10:44:38 +0800

    修正水印类对象无参构造

--- toolbox-core/src/main/java/com/github/hugh/components/watermark/WatermarkLine.java
+++ toolbox-core/src/main/java/com/github/hugh/components/watermark/WatermarkLine.java
@@ -1,6 +1,7 @@
 package com.github.hugh.components.watermark;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -13,6 +14,7 @@ import lombok.Setter;
  */
 @Setter
 @Getter
+@NoArgsConstructor
 public class WatermarkLine {
     /**
      * 文本的键部分，例如“地点”、“海拔”等。
