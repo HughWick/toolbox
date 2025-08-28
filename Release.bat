8610be2541b1faaeae41d7d94f805bbbfa3c0677
HughWick <Dang_Star@live.com>
2025-08-28 15:27:23 +0800

    移除release bat脚本

--- Release.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-
-ECHO "=========RELEASE START===================="
-echo cd %~dp0
-call mvn clean javadoc:jar deploy -P release -Dmaven.test.skip=true -Darguments="gpg.passphrase=10201"
-@REM  call mvn clean deploy -P sonatype-oss-release -Darguments="gpg.passphrase=10201"
-pause 
-
